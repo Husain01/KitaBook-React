aee97b6a76d383faddc533876447d6f6e8d421f6
Husain Bhagat <hussainbhagat52@gmail.com>
2022-03-20 14:39:18 +0530

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Mockman from "mockman-js";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+        <Navbar></Navbar>
       <Routes>
         <Route path="/mock" element={<Mockman/>}/>
       </Routes>
