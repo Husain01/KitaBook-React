9f2c02a830ff84b47bc78c229e38765e394d517d
Husain Bhagat <hussainbhagat52@gmail.com>
2022-03-20 22:09:05 +0530

    Add Product Card component

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './ProductCard.css'
+const ProductCard = ({product={title: ""}}) => {
+    console.log(product.title)
+  return (
+    <div class="card card-ecom  normal-shadow">
+                <div
+                  class="product-img product-img-fill product-img-horizontal"
+                >
+                  <img
+                    src="https://images-na.ssl-images-amazon.com/images/I/41+grDTP2FL._SY344_BO1,204,203,200_.jpg"
+                    alt=""
+                  />
+                </div>
+                <div class="card-footer card-footer-hr">
+                  <div class="product-content product-content-hr">
+                    <div class="product-header">
+                      <h3 class="product-title product-title-hr">
+                        {product.title}
+                      </h3>
+                      <button
+                        class="wishlist wishlist-horizontal normal-shadow"
+                      >
+                        <i class="far fa-heart"></i>
+                      </button>
+                    </div>
+                    <div class="card-badge card-badge-hr">New</div>
+                    <p class="product-desc product-desc-hr">
+                      By Ankur Warikoo
+                    </p>
+                  </div>
+                  <div class="btn-footer">
+                    <span class="price">$39.99</span>
+                    <button class="cart cart-hr normal-shadow">
+                      <i class="fas fa-cart-plus"></i>Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+  )
+}
+
+export default ProductCard
