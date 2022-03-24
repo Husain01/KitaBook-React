455d338e250bed163b4a2dc2a87d16532e00bcf5
Husain Bhagat <hussainbhagat52@gmail.com>
2022-03-24 19:20:48 +0530

    Add Product comp

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Filter from '../../components/Filters/Filter'
+import './Product.css'
 
 const Product = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    try {
+      (async () => {
+        const res = await axios.get('api/products')
+        console.log( res.data.products)
+        setCategories(res.data.products)
+      })();
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
   return (
     <>
     <Filter/>
+    <main className='main-content'>
+    <div className='product-listing'>
+      <div className='products-header'>
+        <h3>Showing All Products</h3>
+      </div>
+    </div>
+    </main>
     </>
     )
 }
