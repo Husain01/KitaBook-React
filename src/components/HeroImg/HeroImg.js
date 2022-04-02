f037ec11b3d4407695e55731083c52568e6bfa2c
Husain Bhagat <hussainbhagat52@gmail.com>
2022-04-02 15:23:32 +0530

    Refactor Home Page for all screens

--- src/components/HeroImg/HeroImg.js
+++ src/components/HeroImg/HeroImg.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import landingPage from '../../assets/landing-page.jpg'
+import mobLanding from '../../assets/landing-mob.png'
+import './HeroImg.css'
 
 const HeroImg = () => {
   return (
@@ -7,7 +9,7 @@ const HeroImg = () => {
     <div class="img-landing ">
     <a href="/pages/product-page/product-page.html">
     <img src={landingPage} alt="" class="img-landing-desktop normal-shadow"/>
-    <img src="/assets/landing-mob.png" alt="" class="img-landing-mob normal-shadow"/>
+    <img src={mobLanding} alt="" class="img-landing-mob normal-shadow"/>
   </a>
   </div>
       </>
